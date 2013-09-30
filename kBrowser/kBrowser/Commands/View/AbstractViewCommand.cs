c33435f010214063c63cda4e9c4f74ac0ac4bb3b
talexu <bjutales@hotmail.com>
2013-09-30 20:06:01 +0800

    update command

---
+++ kBrowser/kBrowser/Commands/View/AbstractViewCommand.cs
@@ -0,0 +1,32 @@
+﻿using kBrowser.Businesses;
+using kBrowser.Models.View;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace kBrowser.Commands.View
+{
+    abstract class AbstractViewCommand : ICommand
+    {
+        protected IDictionary<ViewType, FrameworkElement> _views;
+
+        public AbstractViewCommand(IDictionary<ViewType, FrameworkElement> views)
+        {
+            _views = views;
+        }
+
+        public virtual bool CanExecute(object parameter)
+        {
+            //throw new NotImplementedException();
+            return true;
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        public abstract void Execute(object parameter);
+    }
+}
